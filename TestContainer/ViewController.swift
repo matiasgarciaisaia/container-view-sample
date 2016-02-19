e72d252455a24986fa1977b7d6cd196c69d0cbac
Matias Garcia Isaia <mgarcia@manas.com.ar>
2016-02-19 16:02:53 -0300

    Initial Commit

---
+++ TestContainer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TestContainer
+//
+//  Created by Matías García Isaía on 2/19/16 #8.
+//  Copyright © 2016 Manas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
